0b6a49cc2828cfd1e2a08605b1fde623af0ffdea
kmranrg <kmranrg@gmail.com>
2020-11-15 16:43:42 +0530

    added for loop

---
+++ 10_For_Loop/source.js
@@ -0,0 +1,17 @@
+// Programs
+
+// NOTE: for loop is used when number of iterations is known in advanced
+
+// Program to print multiplication table of  6 using for loop
+
+document.write("<h2>Multiplication Table of 6</h2>");
+for(var i=1; i <= 10; i++){
+    document.write("<h3> 6 x "+ i +" = "+ 6*i +"</h3>");
+}
+
+// Program to print first 5 even numbers i.e. 2,4,6,8 and 10 using for loop
+
+document.write("<h2>First 5 even numbers</h2>");
+for(var i=2; i<=10; i+=2){
+    document.write("<h3>"+ i +"</h3>")
+}
